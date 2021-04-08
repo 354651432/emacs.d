575acac716b3792bdd651959d27a3c7031ac3c67
dual <dual@dr.h>
2021-04-08 17:16:19 +0800

    gofmt config

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,13 +1,15 @@
 (use-package go-mode
   :ensure t
   :mode "\\.go\\'"
-  :hook (go-mode . 'lsp-deferred)
+  :hook
+  (go-mode . 'lsp-deferred)
   :custom
   (compile-command "go build && ./demo")
   (tab-width 4)
   (company-idle-delay 0.3)
   :bind ("C-c C-c" . 'recompile))
 
-(add-hook 'go-mode-hook 'lsp-deferred)
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
 
 (provide 'init-go)
